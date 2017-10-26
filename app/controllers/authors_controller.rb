14e239f66f234b280974e45a0f46f22223945cc1
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-26 16:58:21 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,7 +7,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new(author_params)
     if @author.save
       redirect_to author_path(@author)
     else
